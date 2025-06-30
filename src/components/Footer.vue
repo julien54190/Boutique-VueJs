1f1c0e5b50ee8e9b43942985f1e347cd18b00fe5
julien54190 <lucadello.julien@hotmail.fr>
2025-06-30 14:20:34 +0200

    ajout header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-    <footer>
-        <h1>Footer</h1>
+    <footer class="d-flex align-items-center justify-content-center">
+        <p> julien Copyright&copy; 2025</p> 
     </footer>
 </template>
 
 <style lang="scss" scoped>
-
+    footer {
+        background-color: var(--gray-3);
+        color: var(--text-primary-color);
+    }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts"></script>
 
 <template>
-    <header>
-        <h1>Header</h1>
+    <header class="d-flex flex-row px-20">
+        <a class="d-flex align-items-center mr-20" href="#">
+            <img src="../assets/img/téléchargement.png" alt="" width="50" height="50">
+            <span class="logo">Boutique</span>
+        </a>
+        <ul class="d-flex flex-row flex-fill">
+            <li class="mr-10">
+                <a href="#">Shop</a>
+            </li>
+            <li>
+                <a href="#">Admin</a>
+            </li>
+        </ul>
+        <ul class="d-flex flex-row">
+            <li class="mr-10">
+                <a href="#">Inscription</a>
+            </li>
+            <li>
+                <a href="#">Connexion</a>
+            </li>
+        </ul>
     </header>
 </template>
 
 <style lang="scss" scoped>
+    header {
+        background-color: var(--primary-1);
+
+        a {
+            font-weight: bold;
+            color: var(--text-primary-color);
+
+            &:hover {
+                color: #007bff;
+            }
+        }
+        .logo {
+            font-size: 20px;
+        }
+    }
+
 
 </style>
